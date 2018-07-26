4bce377108c4349522e295b3866510d371a54b97
Kanishka Ganguly <kganguly@logitech.com>
2018-07-26 16:04:51 -0700

    First test commit, reorganize files.

---
+++ quad_garl/controller_manual.py
@@ -0,0 +1,61 @@
+import threading
+import time
+
+import numpy as np
+
+
+class ManualController():
+    def __init__(self, get_state, get_time, actuate_motors, quad_identifier):
+        self.get_state = get_state
+        self.quad_identifier = quad_identifier
+        self.actuate_motors = actuate_motors
+        self.get_time = get_time
+        self.thread_object = None
+        self.target = [0, 0, 0]
+        self.yaw_target = 0.0
+        self.run = True
+        self.MOTOR_LIMITS = [4000, 9000]
+        self.throttle = [0, 0, 0, 0]  # m1, m2, m3, m4
+
+    def wrap_angle(self, val):
+        return ((val + np.pi) % (2 * np.pi) - np.pi)
+
+    def update(self):
+        [dest_x, dest_y, dest_z] = self.target
+        [x, y, z] = self.get_state(self.quad_identifier)
+        x_error = dest_x - x
+        y_error = dest_y - y
+        z_error = dest_z - z
+        m1 = self.throttle[0]
+        m2 = self.throttle[1]
+        m3 = self.throttle[2]
+        m4 = self.throttle[3]
+        M = np.clip([m1, m2, m3, m4], self.MOTOR_LIMITS[0], self.MOTOR_LIMITS[1])
+        print(M)
+        self.actuate_motors(self.quad_identifier, M)
+
+    def update_throttle(self, throttle):
+        self.throttle = throttle
+
+    def update_target(self, target):
+        self.target = target
+
+    def update_yaw_target(self, target):
+        self.yaw_target = self.wrap_angle(target)
+
+    def thread_run(self, update_rate, time_scaling):
+        update_rate = update_rate * time_scaling
+        last_update = self.get_time()
+        while (self.run == True):
+            time.sleep(0)
+            self.time = self.get_time()
+            if (self.time - last_update).total_seconds() > update_rate:
+                self.update()
+                last_update = self.time
+
+    def start_thread(self, update_rate=0.005, time_scaling=1):
+        self.thread_object = threading.Thread(target=self.thread_run, args=(update_rate, time_scaling))
+        self.thread_object.start()
+
+    def stop_thread(self):
+        self.run = False
---
+++ quad_garl/quad_sim_manual.py
@@ -0,0 +1,46 @@
+import signal
+import sys
+
+from quad_garl import controller_manual, gui, quadcopter
+
+
+# Catch Ctrl+C to stop threads
+def signal_handler(signal, frame):
+    global run
+    run = False
+    print('Stopping')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Constants
+TIME_SCALING = 1.0
+QUAD_DYNAMICS_UPDATE = 0.002  # seconds
+CONTROLLER_DYNAMICS_UPDATE = 0.005  # seconds
+run = True
+
+# Define the quadcopters
+QUADCOPTER = {
+    'q1': {'position': [1, 0, 4], 'orientation': [0, 0, 0], 'L': 0.3, 'r': 0.1, 'prop_size': [10, 4.5], 'weight': 1.2}}
+
+# Make objects for quadcopter, gui and controller
+quad = quadcopter.Quadcopter(QUADCOPTER)
+gui_object = gui.GUI(quads=QUADCOPTER)
+ctrl = controller_manual.ManualController(get_state=quad.get_position, get_time=quad.get_time,
+                                          actuate_motors=quad.set_motor_speeds, quad_identifier='q1')
+
+# Start threads
+quad.start_thread(dt=QUAD_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
+ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
+
+# Update the GUI
+throttle = [3000, 3000, 3000, 3000]
+while (run == True):
+    throttle = list(map(lambda x: x + 50, throttle))
+    ctrl.update_throttle(throttle=throttle)
+    gui_object.quads['q1']['position'] = quad.get_position('q1')
+    gui_object.quads['q1']['orientation'] = quad.get_orientation('q1')
+    gui_object.update()
+quad.stop_thread()
+ctrl.stop_thread()
